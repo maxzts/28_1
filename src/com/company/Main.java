d9ea7a31460ee1928d16594f693e6493d964be83
Maxim Zayats <max.zayats2@gmail.com>
2021-03-22 20:53:13 +0300

    first task

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+class hello {
+    public static void main (String[] args ) {
+        for (int i = 7; i > 0; i--) {
+
+            System.out.println(i);
+        }
+    }
+}
